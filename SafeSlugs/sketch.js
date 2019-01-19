28495eda60a0fc8bedf04a69e26413fa97a1c4a0
pablofgaeta <37568237+pablofgaeta@users.noreply.github.com>
2019-01-18 21:30:11 -0800

    Add files via upload

---
+++ SafeSlugs/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB);
+	background(341, 79, 69);
+}
+
+function draw() {
+
+	fill(205,60,60);
+	noStroke();
+	rectMode(CENTER);
+	rect(windowWidth/2,100,windowWidth/2, 100, 20, 20);
+
+	textSize(32);
+	fill(255);
+	textAlign(CENTER);
+	text("Safe Slugs", windowWidth/2, 100, )
+
+}
