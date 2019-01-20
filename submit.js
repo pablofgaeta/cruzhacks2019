9f63fc415c7a11078f9050672164ef24a513c1d2
Margot Uchicua <46686016+muchicua@users.noreply.github.com>
2019-01-20 00:23:59 -0800

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,6 @@
+function submit() {
+  var first = document.getElementById("name").value;
+  var last = document.getElementById("last").value;
+  var number = document.getElementById("number").value;
+  console.log(first);
+}
